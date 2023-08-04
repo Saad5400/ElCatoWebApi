f27bb798c5d4a59eff320d2614e629d5c8b9cfb7
Saad5400 <sdbtwa@gmail.com>
2023-08-04 22:21:02 +0300

    added generating a sitemap

--- ElCatoWebApi/Controllers/UsersController.cs
+++ ElCatoWebApi/Controllers/UsersController.cs
@@ -45,6 +45,42 @@ namespace ElCatoWebApi.Controllers
             return Ok(token);
         }
 
+        [HttpGet]
+        [Route("update-sitemap")]
+        public async Task<IActionResult> UpdateSiteMap()
+        {
+            var baseContent =
+            @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<urlset xmlns=""http://www.sitemaps.org/schemas/sitemap/0.9"">
+    <url>
+        <loc>https://elcato.azurewebsites.net</loc>
+    </url>
+    <url>
+        <loc>https://elcato.azurewebsites.net/admin</loc>
+    </url>
+    <url>
+        <loc>https://elcato.azurewebsites.net/admin/pages</loc>
+    </url>
+";
+
+            var pagesIds = _db.Pages.Select(p => p.Id);
+
+            foreach (var pageId in pagesIds)
+            {
+                baseContent += $@"  <url>
+        <loc>https://elcato.azurewebsites.net/page/{pageId}</loc>
+    </url>
+";
+            }
+            baseContent += "</urlset>";
+
+            var path = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), @"wwwroot\sitemap.xml");
+            var bytes = System.Text.Encoding.UTF8.GetBytes(baseContent);
+            await System.IO.File.WriteAllBytesAsync(path, bytes);
+
+            return Ok("Site Map updated successfully");
+        }
+
         [HttpPost]
         [Authorize]
         [Route("admin")]
