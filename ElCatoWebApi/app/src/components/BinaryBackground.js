82d095a6541b115a46537f7b97d462330613ece7
Saad5400 <sdbtwa@gmail.com>
2023-09-12 11:49:46 +0300

    added stars effect

--- ElCatoWebApi/app/src/components/BinaryBackground.js
+++ ElCatoWebApi/app/src/components/BinaryBackground.js
@@ -3,79 +3,248 @@ import "./BinaryBackground.scss"
 
 export default function BinaryBackground() {
 
-    const speed = 120;
-    const size = 20;
-    const text = "undefined";
-
-    const canvasRef = useRef(null);
-    const frColorRef = useRef(null);
-    const bgColorRef = useRef(null);
-    const [windowWidth, setWindowWidth] = useState(window.outerHeight);
-
-    useEffect(() => {
-        function handleResize() {
-            setWindowWidth(window.innerWidth);
-        }
-
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
+    const firstRender = useRef(true);
 
     useLayoutEffect(() => {
-        if (canvasRef.current) {
-            const canvas = canvasRef.current;
-            const ctx = canvas.getContext('2d');
-            canvas.width = window.innerWidth;
-            canvas.height = window.innerHeight;
-
-            const binary = text;
-            const fontSize = size;
-            const columns = canvas.width / fontSize;
-
-            const drops = [];
-            for (let x = 0; x < columns; x++) {
-                drops[x] = 1;
-            }
-            let bgColor = window?.getComputedStyle(bgColorRef.current)?.color.toString();
-            ctx.fillStyle = bgColor;
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
-            function draw() {
-                let bgColor;
-                try {
-                    bgColor = window?.getComputedStyle(bgColorRef.current)?.color.toString();
-                }
-                catch (err) {
-                    bgColor = "#000000";
-                }
-                bgColor = bgColor.substring(bgColor.indexOf('(') + 1, bgColor.lastIndexOf(')')).split(/,\s*/);
-                ctx.fillStyle = 'rgba(' + bgColor[0] + ', ' + bgColor[1] + ', ' + bgColor[2] + ', 0.05)';
-                ctx.fillRect(0, 0, canvas.width, canvas.height);
-                // ctx.fillStyle =  "#d4163c"
-                ctx.fillStyle =  window?.getComputedStyle(frColorRef.current)?.color;
-                ctx.font = fontSize + 'px Glitch Goblin';
-
-                for (let i = 0; i < drops.length; i++) {
-                    const text = binary[Math.floor(Math.random() * binary.length)];
-                    ctx.fillText(text, i * fontSize, drops[i] * fontSize);
-
-                    if (drops[i] * fontSize > canvas.height && Math.random() > 0.975) {
-                        drops[i] = 0;
-                    }
-
-                    drops[i]++;
-                }
-            }
-            const inter = setInterval(draw, speed);
-
-            return () => clearInterval(inter);
+        if (firstRender.current) {
+            firstRender.current = false;
+            return;
         }
-    }, [windowWidth]);
+        /*          *     .        *  .    *    *   . 
+         .  *  move your mouse to over the stars   .
+         *  .  .   change these values:   .  *
+           .      * .        .          * .       */
+        const STAR_COLOR = '#fff';
+        const STAR_SIZE = 3;
+        const STAR_MIN_SCALE = 0.2;
+        const OVERFLOW_THRESHOLD = 50;
+        const STAR_COUNT = (window.innerWidth + window.innerHeight) / 81;
+
+        const canvas = document.querySelector('canvas'),
+            context = canvas.getContext('2d');
+
+        let scale = 1, // device pixel ratio
+            width,
+            height;
+
+        let stars = [];
+
+        let pointerX,
+            pointerY;
+
+        let velocity = { x: 0, y: 0, tx: 0, ty: 0, z: 0.0005 };
+
+        let touchInput = false;
+
+        generate();
+        resize();
+        step();
+
+        window.onresize = resize;
+        window.onmousemove = onMouseMove;
+        window.ontouchmove = onTouchMove;
+        window.ontouchend = onMouseLeave;
+        document.onmouseleave = onMouseLeave;
+
+        function generate() {
+
+            for (let i = 0; i < STAR_COUNT; i++) {
+                stars.push({
+                    x: 0,
+                    y: 0,
+                    z: STAR_MIN_SCALE + Math.random() * (1 - STAR_MIN_SCALE)
+                });
+            }
+
+        }
+
+        function placeStar(star) {
+
+            star.x = Math.random() * width;
+            star.y = Math.random() * height;
+
+        }
+
+        function recycleStar(star) {
+
+            let direction = 'z';
+
+            let vx = Math.abs(velocity.x),
+                vy = Math.abs(velocity.y);
+
+            if (vx > 1 || vy > 1) {
+                let axis;
+
+                if (vx > vy) {
+                    axis = Math.random() < vx / (vx + vy) ? 'h' : 'v';
+                }
+                else {
+                    axis = Math.random() < vy / (vx + vy) ? 'v' : 'h';
+                }
+
+                if (axis === 'h') {
+                    direction = velocity.x > 0 ? 'l' : 'r';
+                }
+                else {
+                    direction = velocity.y > 0 ? 't' : 'b';
+                }
+            }
+
+            star.z = STAR_MIN_SCALE + Math.random() * (1 - STAR_MIN_SCALE);
+
+            if (direction === 'z') {
+                star.z = 0.1;
+                star.x = Math.random() * width;
+                star.y = Math.random() * height;
+            }
+            else if (direction === 'l') {
+                star.x = -OVERFLOW_THRESHOLD;
+                star.y = height * Math.random();
+            }
+            else if (direction === 'r') {
+                star.x = width + OVERFLOW_THRESHOLD;
+                star.y = height * Math.random();
+            }
+            else if (direction === 't') {
+                star.x = width * Math.random();
+                star.y = -OVERFLOW_THRESHOLD;
+            }
+            else if (direction === 'b') {
+                star.x = width * Math.random();
+                star.y = height + OVERFLOW_THRESHOLD;
+            }
+
+        }
+
+        function resize() {
+
+            scale = window.devicePixelRatio || 1;
+
+            width = window.innerWidth * scale;
+            height = window.innerHeight * scale;
+
+            canvas.width = width;
+            canvas.height = height;
+
+            stars.forEach(placeStar);
+
+        }
+
+        function step() {
+
+            context.clearRect(0, 0, width, height);
+
+            update();
+            render();
+
+            requestAnimationFrame(step);
+
+        }
+
+        function update() {
+
+            velocity.tx *= 0.96;
+            velocity.ty *= 0.96;
+
+            velocity.x += (velocity.tx - velocity.x) * 0.8;
+            velocity.y += (velocity.ty - velocity.y) * 0.8;
+
+            stars.forEach((star) => {
+
+                star.x += velocity.x * star.z;
+                star.y += velocity.y * star.z;
+
+                star.x += (star.x - width / 2) * velocity.z * star.z;
+                star.y += (star.y - height / 2) * velocity.z * star.z;
+                star.z += velocity.z;
+
+                // recycle when out of bounds
+                if (star.x < -OVERFLOW_THRESHOLD || star.x > width + OVERFLOW_THRESHOLD || star.y < -OVERFLOW_THRESHOLD || star.y > height + OVERFLOW_THRESHOLD) {
+                    recycleStar(star);
+                }
+
+            });
+
+        }
+
+        function render() {
+
+            stars.forEach((star) => {
+
+                context.beginPath();
+                context.lineCap = 'round';
+                context.lineWidth = STAR_SIZE * star.z * scale;
+                context.globalAlpha = 0.5 + 0.5 * Math.random();
+                context.strokeStyle = STAR_COLOR;
+
+                context.beginPath();
+                context.moveTo(star.x, star.y);
+
+                var tailX = velocity.x * 2,
+                    tailY = velocity.y * 2;
+
+                // stroke() wont work on an invisible line
+                if (Math.abs(tailX) < 0.1) tailX = 0.5;
+                if (Math.abs(tailY) < 0.1) tailY = 0.5;
+
+                context.lineTo(star.x + tailX, star.y + tailY);
+
+                context.stroke();
+
+            });
+
+        }
+
+        function movePointer(x, y) {
+
+            if (typeof pointerX === 'number' && typeof pointerY === 'number') {
+
+                let ox = x - pointerX,
+                    oy = y - pointerY;
+
+                velocity.tx = velocity.tx + (ox / 8 * scale) * (touchInput ? 1 : -1);
+                velocity.ty = velocity.ty + (oy / 8 * scale) * (touchInput ? 1 : -1);
+
+            }
+
+            pointerX = x;
+            pointerY = y;
+
+        }
+
+        function onMouseMove(event) {
+
+            touchInput = false;
+
+            movePointer(event.clientX, event.clientY);
+
+        }
+
+        function onTouchMove(event) {
+
+            touchInput = true;
+
+            movePointer(event.touches[0].clientX, event.touches[0].clientY, true);
+
+            event.preventDefault();
+
+        }
+
+        function onMouseLeave() {
+
+            pointerX = null;
+            pointerY = null;
+
+        }
+
+
+    }, []);
 
     return (
         <>
-            <canvas ref={canvasRef} id="canvas" className="fixed top-0 left-0 w-screen h-screen z-[-1]"></canvas>
-            <div ref={frColorRef} className="text-base-content"></div>
-            <div ref={bgColorRef} className="text-base-200"></div>
+            <canvas className="fixed top-0 left-0 w-screen h-screen z-[-1]">
+
+            </canvas>
         </>
     )
 }
